07e54b3b53f1dc6ea5fdab4f74b573f2c5d7d6be
MidiAug <2234649224@qq.com>
2024-04-01 15:53:58 +0800

    Revert "帧提取"
    This reverts commit 65e9cf533b9ac31686e1cbfcf2087a30f12ec28a.

--- OurCode/FrameExtractor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <filesystem> //这个需要C++17,右键OurCode，属性->常规->C++标准改为C++17及以上
-
-bool isFrameWhite(const cv::Mat& frame, int threshold, double checkRatio);
-bool FrameExtractor(const std::string& videoPath, const std::string& outputPath, double samplingRatio, int RGBThreshold);
